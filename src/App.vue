45be921efcdddc30ac227f2169d25f5466a35dcd
James Kaviyil Jose <jamesjose03@gmail.com>
2018-08-11 11:24:24 +0530

    Converted to VueJS template. Navbar Done :smiley:

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+  <navbar></navbar>
+  </div>
+</template>
+
+<script>
+import navbar from './components/navbar.vue'
+export default {
+  name: 'app',
+  components: {
+    navbar: navbar
+  }
+}
+</script>
+
+<style>
+/* Color pallette used: #051e3e • #251e3e • #451e3e • #651e3e • #851e3e */ 
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+main {
+  text-align: center;
+  margin-top: 40px;
+}
+
+header {
+  margin: 0;
+  height: 56px;
+  padding: 0 16px 0 24px;
+  background-color: #35495E;
+  color: #ffffff;
+}
+
+header span {
+  display: block;
+  position: relative;
+  font-size: 20px;
+  line-height: 1;
+  letter-spacing: .02em;
+  font-weight: 400;
+  box-sizing: border-box;
+  padding-top: 16px;
+}
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class=jkj-navbar>
+    <nav class="navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand" href="#" style="color: #fff;">JKJ</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+        <li class="nav-item active">
+            <a class="nav-link jkj-home-btn" href="#" style="color: #fff;">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#" style="color: #fff;">About Me</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#" style="color: #fff;">Accomplishments</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#" style="color: #fff;">Projects</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#" style="color: #fff;">Contact</a>
+        </li>
+    </ul>
+  </div>
+</nav>
+    </div>
+</template>
+
+<style>
+.jkj-home-btn{
+    margin-left: 55vw;
+}
+.navbar{
+    background-color: #251e3e;
+    color: #fff;
+}
+.nav-item{
+    margin-right: 1vw;
+    font-family: 'Sunflower', sans-serif;
+    color: #fff;
+}
+.nav-link{
+    color: #fff;
+}
+.navbar-brand{
+    font-family: 'Monoton', cursive;
+    font-size: 1.8em;
+}
+@media screen and (max-width: 992px){
+    .jkj-home-btn{
+        margin-left: 0vw;
+    }
+}
+</style>
